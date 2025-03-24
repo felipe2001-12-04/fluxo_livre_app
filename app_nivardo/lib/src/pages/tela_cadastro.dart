66a45947ecedf8f1d71919a85ab21c1d077337cc
felipe <felipe.estorari@sou.unifeob.edu.br>
2025-03-24 19:38:39 -0300

    commit do projeto

---
+++ app_nivardo/lib/src/pages/tela_cadastro.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastro extends StatelessWidget {
+  const TelaCadastro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
